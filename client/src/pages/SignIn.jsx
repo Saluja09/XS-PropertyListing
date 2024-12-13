242bcaf366ae278d999681a76606a49671ed5f4a
ARYAMAN SALUJA <91595380+Saluja09@users.noreply.github.com>
2024-12-13 18:43:52 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -19,7 +19,7 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('http://localhost:7000/api/auth/signin', {
+      const res = await fetch('https://realestate-gxcu.onrender.com/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
